963c1878c06491bcd295e40944127c07b4d72a1c
Quyson Nguyen <Quyson.nguyen.1999@gmail.com>
2023-03-27 16:08:55 -0700

    Create protected database link and Hook up to database

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const passport = require('passport');
+require('dotenv').config();
+
+const dbURI = process.env.dbURI;
+console.log(dbURI)
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then((result) => console.log('Connected to DB'))
+	.catch((error) => console.log(error))
+    .then((result) => app.listen(8000));
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(cors());
