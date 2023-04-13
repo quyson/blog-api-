b056cad088b9460fd0cc8d1065df27482cc126d6
Quyson Nguyen <Quyson.nguyen.1999@gmail.com>
2023-04-12 22:23:28 -0700

    removed .env file from view

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,99 +1,107 @@
-const User = require('../models/userModel');
-const Post = require('../models/postModel');
-const Comment = require('../models/commentModel');
+const User = require("../models/userModel");
+const Post = require("../models/postModel");
+const Comment = require("../models/commentModel");
 
 const getUsers = (req, res) => {
-    User.find()
-    .sort({name: 1})
+  User.find()
+    .sort({ name: 1 })
     .then((result) => {
+      // this loop will remove password from user in result to prevent sending unnecessary information to client
 
-        // this loop will remove password from user in result to prevent sending unnecessary information to client
-
-        const resultArray = [];
-        result.forEach((element) => {
-            let objectHolder = {
-                _id: element._id,
-                username: element.username,
-                first_name: element.first_name,
-                last_name: element.last_name
-            };
-            resultArray.push(objectHolder)
-        });
-        res.send({
-            success: true,
-            result: resultArray
-        });
+      const resultArray = [];
+      result.forEach((element) => {
+        let objectHolder = {
+          _id: element._id,
+          username: element.username,
+          first_name: element.first_name,
+          last_name: element.last_name,
+        };
+        resultArray.push(objectHolder);
+      });
+      res.send({
+        success: true,
+        result: resultArray,
+      });
     })
     .catch((err) => {
-        return next(err);
+      return next(err);
     });
 };
 
 const oneUser = async (req, res) => {
-    try{
-        let results = await Promise.all([
-            User.findById(req.params.id, ['username', 'first_name', 'last_name', 'bio']),
-            Post.find({user: req.params.id})
-        ]);
-        const resultUser = results[0];
-        const resultPost = results[1];
-        console.log(resultUser)
-        console.log(resultPost);
-        res.send({
-            success: true,
-            resultUser: resultUser,
-            resultPost: resultPost,
-            currentUser: req.user
-
-        });
-    } catch(error) {
-        res.send({
-            success: false,
-            message: "User not found."
-        })
-    };
+  try {
+    let results = await Promise.all([
+      User.findById(req.params.id, [
+        "username",
+        "first_name",
+        "last_name",
+        "bio",
+      ]),
+      Post.find({ user: req.params.id }),
+    ]);
+    const resultUser = results[0];
+    const resultPost = results[1];
+    console.log(resultUser);
+    console.log(resultPost);
+    res.send({
+      success: true,
+      resultUser: resultUser,
+      resultPost: resultPost,
+      currentUser: req.user,
+    });
+  } catch (error) {
+    res.send({
+      success: false,
+      message: "User not found.",
+    });
+  }
 };
 
 const updateUser = (req, res) => {
-    User.findByIdAndUpdate(req.params.id, req.body, {new: true}).then((result) => {
-        res.status(200).send({
-            success: true,
-            result: result
-        });
+  User.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    .then((result) => {
+      res.status(200).send({
+        success: true,
+        result: result,
+      });
     })
     .catch((err) => {
-        res.status(400).send({
-            success: false,
-            message: "Unable to update user."
-        });
+      res.status(400).send({
+        success: false,
+        message: "Unable to update user.",
+      });
     });
 };
 
 const deleteUser = (req, res) => {
-    const messageObj = {}
-    Comment.deleteMany({user: req.params.id}).then((result) => {
-        messageObj.commentMsg = "All related comments deleted!"
+  const messageObj = {};
+  Comment.deleteMany({ user: req.params.id })
+    .then((result) => {
+      messageObj.commentMsg = "All related comments deleted!";
     })
     .catch((err) => {
-        messageObj.commentMsg = "Unable to delete related comments."
+      messageObj.commentMsg = "Unable to delete related comments.";
     });
-    Post.deleteMany({user: req.params.id}).then((result) => {
-        messageObj.postMsg = "All related Posts deleted!"
+  Post.deleteMany({ user: req.params.id })
+    .then((result) => {
+      messageObj.postMsg = "All related Posts deleted!";
+      console.log(result);
     })
     .catch((err) => {
-        messageObj.postMsg = "Unable to delete related Posts."
+      messageObj.postMsg = "Unable to delete related Posts.";
     });
-    User.findByIdAndDelete(req.params.id).then((result) => {
-        messageObj.success = true;
-        messageObj.message = "Successfully Deleted User!";
-        res.status(200).send(messageObj);
+  User.findByIdAndDelete(req.params.id)
+    .then((result) => {
+      messageObj.success = true;
+      messageObj.message = "Successfully Deleted User!";
+      res.status(200).send(messageObj);
     })
     .catch((err) => {
-        res.status(401).send({
-            success: false,
-            message: "Unable to Delete User."
-        });
+      res.status(401).send({
+        success: false,
+        message: "Unable to Delete User.",
+      });
     });
 };
 
-module.exports = {getUsers, oneUser, deleteUser, updateUser}
+module.exports = { getUsers, oneUser, deleteUser, updateUser };
--- server.js
+++ server.js
@@ -1,26 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const mongoose = require('mongoose');
-const morgan = require('morgan');
-const passport = require('passport');
-require('dotenv').config();
-const apiRouter = require('./routes/apiRoutes');
+const cors = require("cors");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const passport = require("passport");
+require("dotenv").config();
+const apiRouter = require("./routes/apiRoutes");
 
 const dbURI = process.env.dbURI;
 
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-	.then((result) => console.log('Connected to DB'))
-	.catch((error) => console.log(error))
-    .then((result) => app.listen(8000));
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => console.log("Connected to DB"))
+  .catch((error) => console.log(error))
+  .then((result) => app.listen(9000));
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use(passport.initialize());
-require('./config/passport');
+require("./config/passport");
 
 app.use(apiRouter);
-
